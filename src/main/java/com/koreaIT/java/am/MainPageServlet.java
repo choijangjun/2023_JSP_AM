0acd285f34eae6c681764a5c5295057eabf77fc9
choijangjun <jangjun9644@gmail.com>
2023-04-10 17:40:54 +0900

    게시물 작성시 작성자에 관한 정보 저장

--- src/main/java/com/koreaIT/java/am/MainPageServlet.java
+++ src/main/java/com/koreaIT/java/am/MainPageServlet.java
@@ -1,28 +1,30 @@
 package com.koreaIT.java.am;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
-
-import com.koreaIT.java.am.util.DBUtil;
-import com.koreaIT.java.am.util.SecSql;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/home/main")
 public class MainPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		
+		int loginedMemberId = -1;
+		
+		if (session.getAttribute("loginedMemberId") != null) {
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
+		}
+		
+		request.setAttribute("loginedMemberId", loginedMemberId);
+		
 		request.getRequestDispatcher("/jsp/home/main.jsp").forward(request, response);
-		
-		
 	}
 }
